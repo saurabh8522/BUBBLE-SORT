fb4e960a00a0ce09ea5f4ee568641638242bba91
joint2021 <56085399+joint2021@users.noreply.github.com>
2020-10-05 20:50:15 +0530

    Update LITE.cpp

--- LITE.cpp
+++ LITE.cpp
@@ -1,119 +1,198 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define mod 100000009
-#define ll long long int
-struct bulb{
-	ll on,off;
-};
-//building segmnet tree
-//below is the complete code
-void build(struct bulb *seg,ll *lazy,ll node,ll start,ll end){
-	//cout<<node<<endl;
-	if(start==end){
-		seg[node].on=0;
-		seg[node].off=1;
-		lazy[node]=0;
-		return ;
-	}
-	ll mid=(start+end)/2;
-	build(seg,lazy,2*node,start,mid);
-	build(seg,lazy,2*node+1,mid+1,end);
-	seg[node].on=0;
-	seg[node].off=seg[2*node].off+seg[2*node+1].off;
-	lazy[node]=0;
-}
-// range update
-void update(struct bulb *seg,ll *lazy,ll node,ll start,ll end,ll l,ll r){
-	if(lazy[node]!=0){
-		if(lazy[node]&1){
-			ll temp=seg[node].on;
-			seg[node].on=seg[node].off;
-			seg[node].off=temp;
-		}
-		if(start!=end){
-			lazy[2*node+1]+=lazy[node];
-			lazy[2*node]+=lazy[node];
-		}
-		lazy[node]=0;
-	}
-	if(r<start||l>end){
-		return ;
-	}
-	if(start>=l&&end<=r){
-		ll temp=seg[node].on;
-		seg[node].on=seg[node].off;
-		seg[node].off=temp;
-		if(start!=end){
-			lazy[2*node]++;
-			lazy[2*node+1]++;
-		}
-		return ;
-	}
-	ll mid=(start+end)/2;
-	update(seg,lazy,2*node,start,mid,l,r);
-	update(seg,lazy,2*node+1,mid+1,end,l,r);
-	seg[node].on=seg[2*node].on+seg[2*node+1].on;
-	seg[node].off=seg[2*node].off+seg[2*node+1].off;
-}
+// C++ program to insert a node in AVL tree 
+#include<bits/stdc++.h> 
+using namespace std; 
 
-//bulb function
-struct bulb find(struct bulb *seg,ll *lazy,ll node,ll start,ll end,ll l,ll r){
-	struct bulb ans;
-	if(r<start||l>end){
-		ans.on=0;
-		ans.off=0;
-		return ans;
-	}
-	if(lazy[node]!=0){
-		if(lazy[node]&1){
-			ll temp=seg[node].on;
-			seg[node].on=seg[node].off;
-			seg[node].off=temp;
-		}
-		if(start!=end){
-			lazy[2*node+1]+=lazy[node];
-			lazy[2*node]+=lazy[node];
-		}
-		lazy[node]=0;
-	}
-	if(start>=l&&end<=r){
-		return seg[node];
-	}
-	ll mid=(start+end)/2;
-	struct bulb ans1=find(seg,lazy,2*node,start,mid,l,r);
-	struct bulb ans2=find(seg,lazy,2*node+1,mid+1,end,l,r);
-	ans.on=ans1.on+ans2.on;
-	ans.off=ans1.off+ans2.off;
-	return ans;
-}
-//main
-int main(){
-	ll n,q;
-	scanf("%lld%lld",&n,&q);
-	struct bulb seg[4*n];
-	ll lazy[4*n];
-	build(seg,lazy,1,1,n);
-	//cout<<0<<endl;
-	// for(ll i=1;i<=2*n-1;i++){
-	// 		cout<<seg[i].on<<" "<<seg[i].off<<" "<<lazy[i]<<endl;
-	// 	}
-	// 	cout<<endl;
-	while(q--){
-		ll type;
-		scanf("%lld",&type);
-		if(type==0){
-			ll l,r;
-			scanf("%lld%lld",&l,&r);
-			update(seg,lazy,1,1,n,l,r);
-			// for(ll i=1;i<=2*n-1;i++){
-			// 	cout<<seg[i].on<<" "<<seg[i].off<<" "<<lazy[i]<<endl;
-			// }
-			// cout<<endl;
-		}
-		else{
-			ll l,r;
-			scanf("%lld%lld",&l,&r);
-			printf("%lld\n",find(seg,lazy,1,1,n,l,r).on);
-		}
-	}
-}
+// An AVL tree node 
+class Node 
+{ 
+	public: 
+	int key; 
+	Node *left; 
+	Node *right; 
+	int height; 
+}; 
+
+// A utility function to get maximum 
+// of two integers 
+int max(int a, int b); 
+
+// A utility function to get the 
+// height of the tree 
+int height(Node *N) 
+{ 
+	if (N == NULL) 
+		return 0; 
+	return N->height; 
+} 
+
+// A utility function to get maximum 
+// of two integers 
+int max(int a, int b) 
+{ 
+	return (a > b)? a : b; 
+} 
+
+/* Helper function that allocates a 
+new node with the given key and 
+NULL left and right pointers. */
+Node* newNode(int key) 
+{ 
+	Node* node = new Node(); 
+	node->key = key; 
+	node->left = NULL; 
+	node->right = NULL; 
+	node->height = 1; // new node is initially 
+					// added at leaf 
+	return(node); 
+} 
+
+// A utility function to right 
+// rotate subtree rooted with y 
+// See the diagram given above. 
+Node *rightRotate(Node *y) 
+{ 
+	Node *x = y->left; 
+	Node *T2 = x->right; 
+
+	// Perform rotation 
+	x->right = y; 
+	y->left = T2; 
+
+	// Update heights 
+	y->height = max(height(y->left), 
+					height(y->right)) + 1; 
+	x->height = max(height(x->left), 
+					height(x->right)) + 1; 
+
+	// Return new root 
+	return x; 
+} 
+
+// A utility function to left 
+// rotate subtree rooted with x 
+// See the diagram given above. 
+Node *leftRotate(Node *x) 
+{ 
+	Node *y = x->right; 
+	Node *T2 = y->left; 
+
+	// Perform rotation 
+	y->left = x; 
+	x->right = T2; 
+
+	// Update heights 
+	x->height = max(height(x->left),	 
+					height(x->right)) + 1; 
+	y->height = max(height(y->left), 
+					height(y->right)) + 1; 
+
+	// Return new root 
+	return y; 
+} 
+
+// Get Balance factor of node N 
+int getBalance(Node *N) 
+{ 
+	if (N == NULL) 
+		return 0; 
+	return height(N->left) - height(N->right); 
+} 
+
+// Recursive function to insert a key 
+// in the subtree rooted with node and 
+// returns the new root of the subtree. 
+Node* insert(Node* node, int key) 
+{ 
+	/* 1. Perform the normal BST insertion */
+	if (node == NULL) 
+		return(newNode(key)); 
+
+	if (key < node->key) 
+		node->left = insert(node->left, key); 
+	else if (key > node->key) 
+		node->right = insert(node->right, key); 
+	else // Equal keys are not allowed in BST 
+		return node; 
+
+	/* 2. Update height of this ancestor node */
+	node->height = 1 + max(height(node->left), 
+						height(node->right)); 
+
+	/* 3. Get the balance factor of this ancestor 
+		node to check whether this node became 
+		unbalanced */
+	int balance = getBalance(node); 
+
+	// If this node becomes unbalanced, then 
+	// there are 4 cases 
+
+	// Left Left Case 
+	if (balance > 1 && key < node->left->key) 
+		return rightRotate(node); 
+
+	// Right Right Case 
+	if (balance < -1 && key > node->right->key) 
+		return leftRotate(node); 
+
+	// Left Right Case 
+	if (balance > 1 && key > node->left->key) 
+	{ 
+		node->left = leftRotate(node->left); 
+		return rightRotate(node); 
+	} 
+
+	// Right Left Case 
+	if (balance < -1 && key < node->right->key) 
+	{ 
+		node->right = rightRotate(node->right); 
+		return leftRotate(node); 
+	} 
+
+	/* return the (unchanged) node pointer */
+	return node; 
+} 
+
+// A utility function to print preorder 
+// traversal of the tree. 
+// The function also prints height 
+// of every node 
+void preOrder(Node *root) 
+{ 
+	if(root != NULL) 
+	{ 
+		cout << root->key << " "; 
+		preOrder(root->left); 
+		preOrder(root->right); 
+	} 
+} 
+
+// Driver Code 
+int main() 
+{ 
+	Node *root = NULL; 
+	
+	/* Constructing tree given in 
+	the above figure */
+	root = insert(root, 10); 
+	root = insert(root, 20); 
+	root = insert(root, 30); 
+	root = insert(root, 40); 
+	root = insert(root, 50); 
+	root = insert(root, 25); 
+	
+	/* The constructed AVL Tree would be 
+				30 
+			/ \ 
+			20 40 
+			/ \ \ 
+		10 25 50 
+	*/
+	cout << "Preorder traversal of the "
+			"constructed AVL tree is \n"; 
+	preOrder(root); 
+	
+	return 0; 
+} 
+
+// This code is contributed by 
+// rathbhupendra 
